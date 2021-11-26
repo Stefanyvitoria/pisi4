83dd7d3b4f0f0afdd91fc3d03187d2f79149c024
Stefany Izidio <stefanyvitoria9307@gmail.com>
2021-11-25 23:37:21 -0300

    - armazenando no s3 e recuperando.

--- src/app.py
+++ src/app.py
@@ -3,14 +3,20 @@ from os import system
 from flask import Flask, request, render_template, send_file
 import boto3
 
-KEY_ID = "ASIAVF37VDXB6VDQZF6K"
-KEY_SECRET = "cfC+zwbAPDIkcaXKHtu6PgM7t9bPE2vlwcvcVNv5"
-TOKEN = "FwoGZXIvYXdzELT//////////wEaDHDDakd+Npo7Il6PayLUAcZpo2hVbcx2FlMYVeM7dCiZAl/Cky63voEIivHmmp1GAICEyP6TeihnSsRROKCoy6M/4l2YL3lYnbc8joOslo7emgKDHzjfGpg443nMB9pg6wOFKnIcKXRR55XqybX6paNA+Uju0BkJ1inRNshqbn3BTlgCTkNfcXa1c7qryKLsxpKhyoDC7yrp9yACKff1bhvBBsk1HwBCk/wTSHcLBj4ZptDzYq3X0YASdPN9CNmexU40I/k8/OhdaHoBBprPPwzkIZw5vagkx6AXyFPMhURxsVQwKLz4+4wGMi3FX7EzfnKARVYhJXeqlW+laTy0C1M5Jb7bhN6Cf+kCYx3C24KMFm5LZ0lIKY8="
+KEY_ID = "ASIAVF37VDXBV4PUBLOJ"
+KEY_SECRET = "JBk//tklrsaaPGT/yZoHPOMqDEpOT/QsCnijAm1I"
+TOKEN = "FwoGZXIvYXdzEMv//////////wEaDGFxgrMUrKE228IDuiLUAbw4UK9yVpF7MJ7Q11zPQM0khbJos+RY8bWeQT+DnBztC4KCtyR1ME4v0vdyDfknY0MGvMYVtScHkIra9yIr9qMIenc9SJ+r4oxsdjI1jxgRGGii6bt7dLP5HO8rHzI5KTe5G9oKQDUxjvbTtKIdkbdmHCcznDr2wT5e90KOvZMVszdYDa6Vn9XKgka7ik2r12I2Tar4JeGnN3UeS6JUrGH2W/P/7j89ITYlEKOXN0OWX28J9ZKEdieLiV/RArmALfykbVatgjcOzTRFbJVfk91lTbmoKIr6gI0GMi2EAKKQKaPGqfjWWFYy/t06v4VyxeoftzXPPPQqu3LRNg4EB27vg1bEc033L54="
 
 textractcliente = boto3.client("textract", aws_access_key_id=KEY_ID, aws_secret_access_key=KEY_SECRET, region_name="us-east-1", aws_session_token=TOKEN)
 s3client = boto3.client('s3', aws_access_key_id=KEY_ID, aws_secret_access_key=KEY_SECRET, region_name="us-east-1", aws_session_token=TOKEN)
 pollyclient = boto3.client('polly', aws_access_key_id=KEY_ID, aws_secret_access_key=KEY_SECRET, region_name="us-east-1", aws_session_token=TOKEN)
 
+class ObjFile():
+    def __init__(self, txt):
+        self.txt = txt
+
+    def read(self, *args):
+        return str.encode(self.txt)
 
 
 app = Flask(__name__)
@@ -29,20 +35,20 @@ def js():
     return send_file('../assets/js/script.js', mimetype='text/js')
 
 
+
 @app.route("/", methods=["GET", "POST", "PUT"])
 def main():
     return render_template('index.html')
 
+@app.route("/reprodutor", methods=["GET", "POST", "PUT"])
+def reprodutor():
+    return render_template('reproducao.html')
+
 @app.route("/extract", methods=["POST"])
 def extractImage():
-
     file = request.files.get("file")
     binaryFile = file.read()
-    response = textractcliente.detect_document_text(
-        Document = {
-            "Bytes": binaryFile
-        }
-    )
+    response = textractcliente.detect_document_text(Document = {"Bytes": binaryFile})
     text = ""
     for block in response["Blocks"]:
         if (block["BlockType"] == "LINE"):
@@ -50,26 +56,24 @@ def extractImage():
     file = open('text.txt', mode='w')
     file.write(text)
     file.close()
-
     return text
 
 @app.route("/upfile", methods=["POST"])
 def upFile():
     response = s3client.upload_file('./text.txt', 'stefany-txt-bucket', 'hello.txt')
-
     return response
 
 @app.route("/createbucket", methods=["POST"])
 def createBucket():
     response = s3client.create_bucket(Bucket='stefany-txt-bucket-teste')
-
     return response
 
 @app.route("/polly", methods=["POST"])
 def polly():
+    txt = request.args.get('txt')
     response = pollyclient.synthesize_speech(
     OutputFormat='mp3',
-    Text='hello, how are you?',
+    Text=txt,
     VoiceId='Amy'
     )
 
@@ -78,44 +82,41 @@ def polly():
     file.close()
     return response['ResponseMetadata']['RequestId']
 
-
 @app.route('/aws', methods=["GET", "POST"])
 def aws():
     file = request.files.get("file")
 
     #Extração do texto
     binaryFile = file.read()
-    response = textractcliente.detect_document_text(
-        Document = {
-            "Bytes": binaryFile
-        }
-    )
+    response = textractcliente.detect_document_text(Document = {"Bytes": binaryFile})
     text = ""
     for block in response["Blocks"]:
         if (block["BlockType"] == "LINE"):
             text+= block["Text"]+' '
-    file = open('text.txt', mode='w')
-    file.write(text)
-    file.close()
 
     #Armazenando o texto
-    response = s3client.upload_file('./text.txt', 'stefany-txt-bucket', 'texto-extraido.txt')
+    text = ObjFile(text)
+    response = s3client.upload_fileobj(text, 'stefany-txt-bucket', 'texto-extraido.txt')
 
     #Criando Audio
     response = pollyclient.synthesize_speech(
-    OutputFormat='mp3',
-    Text=text,
-    VoiceId='Amy'
+        OutputFormat='mp3',
+        Text=text.txt,
+        VoiceId='Amy'
     )
 
-    file = open("voice.mp3", 'wb')
-    file.write(response['AudioStream'].read())
-    file.close()
-
-    response = s3client.upload_file('./voice.mp3', 'stefany-polly-bucket', 'texto-falado.mp3')
-
+    #Armazenando o audio
+    response = s3client.upload_fileobj(response['AudioStream'], 'stefany-polly-bucket', 'texto-falado.mp3')
     return render_template('reproducao.html')
 
+@app.route('/voice', methods=["GET", "POST"])
+def voice():
+    # Recuperando o audio
+    response = s3client.get_object(
+        Bucket='stefany-polly-bucket',
+        Key='texto-falado.mp3'
+    )
+    return send_file(response['Body'], mimetype='audio/mp3')
 
 
 app.run("127.0.0.1", port="5000", debug=True)
